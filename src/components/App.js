763962885c777a02c3a9384fed6ba1adfaf9aa45
Zach Saffrin <zach@zachsaffrin.com>
2017-09-11 16:49:04 -0400

    Split header and data into separate components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import Header from './Header';
+import ProductInfoTable from './ProductInfoTable';
+
 require('overeasy/dist/overeasy.min.css');
 require('font-awesome/css/font-awesome.min.css');
 require('../styles/App.scss');
 
-const cropData = require('../data/products.json');
+const products = require('../data/products.json');
 
 const App = () => (
 	<div id="app" className="p2">
-		<h1>HayDay-Manager</h1>
+		<Header />
 
-		<p>{cropData.length} products loaded</p>
+		<ProductInfoTable products={products} />
 	</div>
 );
 
